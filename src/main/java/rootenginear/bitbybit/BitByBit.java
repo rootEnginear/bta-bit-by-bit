d4171b9f97967c4e397c39845035465f845a8726
rootEnginear <rootEnginear@users.noreply.github.com>
2023-09-05 12:48:32 +0700

    feat: add files

--- src/main/java/rootenginear/bitbybit/BitByBit.java
+++ src/main/java/rootenginear/bitbybit/BitByBit.java
@@ -1,16 +1,15 @@
-package turniplabs.examplemod;
+package rootenginear.bitbybit;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "examplemod";
+public class BitByBit implements ModInitializer {
+    public static final String MOD_ID = "bitbybit";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("Bit by Bit initialized.");
     }
 }
